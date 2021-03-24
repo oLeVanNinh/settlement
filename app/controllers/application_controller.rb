a44f2ade077b4eca4d770d43a71c2d1436b12a5a
Ninh <le.van.ninh@sun-asterisk.com>
2021-03-24 11:48:36 +0700

    Skip filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
   def all
     Rails.logger.info "---------------- request ------------------"
     Rails.logger.info params.inspect
