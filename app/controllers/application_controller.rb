a14dcdb32ea33c3b9dd983de7eda8a21ad34cc99
Ninh <le.van.ninh@sun-asterisk.com>
2021-05-10 16:53:11 +0700

    Change mess

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
         Rails.cache.write(:receipts, receipts)
       end
     end
+    raise StandardError
     render plain: "EK", layout: false
   end
 
